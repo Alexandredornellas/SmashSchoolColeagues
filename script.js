575655e11dc60e7725e969f9daf0ccace87b5d83
yanofelix <devyanfelix@gmail.com>
2023-06-12 23:16:15 -0300

    martelada working

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 //Função para fazer o martelo bater
-var hammer = document.getElementById("player")
+const hammer = document.getElementById("player")
+const hammerContainerEl = document.querySelector(".hammerContainer")
+const pontuacao = document.getElementById("pontuacao");
+hammerContainerEl.style.justifyContent = "center"
+hammerContainerEl.style.alignItems = "center"
+var alvo = null;
+var hits = 0;
 
-hammer.addEventListener("click", function(){
+const possibilidades = {
+  "flex-startflex-start": 1,
+  "centerflex-start": 2,
+  "flex-endflex-start": 3,
+  "flex-startcenter": 4,
+  "centercenter": 5,
+  "flex-endcenter": 6,
+  "flex-startflex-end": 7,
+  "centerflex-end": 8,
+  "flex-endflex-end": 9,
+}
+hammer.addEventListener("click", function(e){
   hammer.style.backgroundImage = "url('./imgs/hammer_smashing.png')"
+  const hammerHitPosition = hammerContainerEl.style.justifyContent + hammerContainerEl.style.alignItems
+
+  console.log(possibilidades[hammerHitPosition])
+  console.log(alvo)
+  if(possibilidades[hammerHitPosition] == alvo){
+    hits += 1
+    console.log("hits: " + hits)
+    pontuacao.append(hits);
+  }
 
   setTimeout(function() {
     hammer.style.backgroundImage = "url('./imgs/hammer_fronta.png')"
@@ -28,4 +54,23 @@ function updateHCssProperty(value){
   //altera a propriedade esoclhida
   element.style.justifyContent = value;
 
+}
+
+function startGame(){
+  setInterval(() => {
+    const hitSpot = document.getElementById(Math.floor((Math.random() * 9) + 1));
+    hitSpot.style.backgroundColor = "red";
+    alvo = hitSpot.id;
+    console.log(alvo)
+    setTimeout(() => {
+      hitSpot.style.backgroundColor = "blue";
+      alvo = null;
+    }, 3000);
+  }, 5000);
+//hitSpot
+
+}
+
+function captureClickHit(){
+  
 }
